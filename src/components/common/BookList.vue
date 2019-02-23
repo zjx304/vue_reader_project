6f8c78afa2cdfa29c9652fd3ccae69ba27cbaecb
zjx304 <2920411579@qq.com>
2019-02-23 19:03:45 +0800

    修改筛选栏

--- src/components/common/BookList.vue
+++ src/components/common/BookList.vue
@@ -4,14 +4,14 @@
       <li v-for="(book) in bookList" :key="book._id">
         <div class="book-list-lft"></div>
         <div class="book-list-right">
-          <h3>{{book.title}}{{book._id}}</h3>
+          <h3>{{book.title}}</h3>
           <div class="book-des">
             {{book.shortIntro}}
           </div>
           <div class="book-info">
             <div class="author">{{book.author}}</div>
             <div class="label">
-
+              
             </div>
           </div>
         </div>
--- src/store/state.js
+++ src/store/state.js
@@ -1,6 +1,12 @@
 const state = {
   id:'',
   selectTab:'',
+  filter:{           //分类筛选
+    gender: 'male',     //性别
+    type: 'hot',    //类型
+    major: '奇幻',      //主分类
+    minor: '',      //子分类
+  },
 }
 
 export default state;
